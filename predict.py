e469bb617fdebdb5a1a07d9556242e7ae566e804
pratikdev7 <pratik_edu@outlook.com>
2023-09-14 22:50:19 +0200

    new model

--- predict.py
+++ predict.py
@@ -8,8 +8,8 @@ with open('RoboFlowAPIKey.txt', 'r') as f:
 def predict(image_path):
     rf = Roboflow(api_key=key)
     project = rf.workspace().project(
-        "traffic-detection-ps27b")
-    model = project.version(1).model
+        "thesis-wf3lz")
+    model = project.version(3).model
     prediction = model.predict(image_path, confidence=40, overlap=30).json()
     model.predict(image_path, confidence=40, overlap=30).save(
         "savedprediction.jpg")
