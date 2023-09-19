f21e7ad7d1a618ca8691cdd9060d3c6fb642672d
pratikdev7 <pratik_edu@outlook.com>
2023-09-19 12:19:49 +0200

    Updated the algorithm

--- trafficSwitchAlgo.py
+++ trafficSwitchAlgo.py
@@ -10,23 +10,32 @@ def trafficSwitch(prediction):
         # Create a Counter object
     counter = dict(Counter(classList))
     # print(type(dict(counter)))
-    # print(counter)
+    print(counter)
     for objType, totalCount in counter.items():
         # print("{} {} detected in the image".format(totalCount, objType))
         st.write("{} {} detected in the image".format(totalCount, objType))
 
     # print("counter length: " + str(len(counter)))
     if len(counter) != 0:
+
         for item, count in counter.items():
+            print(item+''+str(count))
             if (item == 'ambulance' or item == 'firetruck') and count >= 1:
                 light_color = 'red'
                 # print("reeeeeeeeeeeed")
+                break
             elif (item == 'Elderly Person' or item == 'Wheelchair' or item == 'peopleWithWheelchair' or item == 'not wheel chair') and count >= 1:
                 light_color = 'green'
+                print("herererereeeeeeee")
+                print(light_color)
+                break
             elif (item == 'person' or item == 'people ') and count >= 2:
+                print("herere????")
                 light_color = 'green'
+                break
             else:
                 light_color = 'red'
+                print("haha red")
     else:
         light_color = 'green'
 
