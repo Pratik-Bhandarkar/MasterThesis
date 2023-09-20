517ff8b83bea29df393bbda62e1dec01fe8cb866
pratikdev7 <pratik_edu@outlook.com>
2023-09-20 10:52:38 +0200

    Updated codes

---
+++ yolov5.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+if __name__ == '__main__':
+    # Load a model
+    model = YOLO("yolov5x.pt")  # load a pretrained model
+
+    # Use the model
+    results = model.train(data='/content/drive/My Drive/Colab Notebooks/data.yaml',
+                          imgsz=640,
+                          epochs=20,
+                          batch=4,
+                          name='yolov8n_custom')  # train the model
+    results = model.val()
